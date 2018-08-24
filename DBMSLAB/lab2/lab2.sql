67b476f6edb2e39bda8b5b8659ffa25437f20065
dashan124 <dashanjotsingh961999@gmail.com>
2018-08-24 10:02:14 +0530

    dbms

--- DBMSLAB/lab2/lab2.sql
+++ DBMSLAB/lab2/lab2.sql
@@ -39,7 +39,7 @@ create table if not exists DEPENDENT (
 	Sex char NOT NULL,
 	Bdate date NOT NULL,
 	Relationship varchar(50) NOT NULL,	
-	PRIMARY KEY(Dependent_name),
+	-- PRIMARY KEY(Dependent_name),
 	FOREIGN KEY(Essn) REFERENCES EMPLOYEE(Ssn)
 );
 create table if not exists DEPT_LOCATIONS(
@@ -113,4 +113,10 @@ insert into WORKS_ON values(888665555,20,NULL);
 
 
 
-
+insert into DEPENDENT values(333445555,'Alice','F','1986-04-05','Daughter');
+insert into DEPENDENT values(333445555,'Theodore','M','1983-10-25','Son');
+insert into DEPENDENT values(333445555,'Joy','F','1958-05-03','Spouse');
+insert into DEPENDENT values(987654321,'Abner','M','1942-02-28','Spouse');
+insert into DEPENDENT values(123456789,'Michael','M','1988-01-04','Son');
+insert into DEPENDENT values(123456789,'Alice','F','1988-12-30','Daughter');
+insert into DEPENDENT values(123456789,'Elizabeth','F','1967-05-05','Spouse');
