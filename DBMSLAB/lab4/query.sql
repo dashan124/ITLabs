64901abb5c8cb5f0a802a75f549ab249c40a0edd
dashan124 <dashanjotsingh961999@gmail.com>
2018-10-05 10:20:12 +0530

    upsdated

--- DBMSLAB/lab4/query.sql
+++ DBMSLAB/lab4/query.sql
@@ -1,21 +1,17 @@
+-- 1 query 
 select t.name as "name of patient",
     p.name as "name of physicain"
     from PATIENT t 
     join PHYSICIAN p on p.employeeid=t.pcp
     where t.pcp not in (select head from DEPARTMENT);
 
-
+ -- 2 query 
 select pt.name, ph.name 
 from PATIENT pt, PHYSICIAN ph 
 WHERE pt.pcp = ph.employeeid and 2 <= (select count(a.appointmentid) from APPOINTMENT a, NURSE n 
 where a.prepnurse = n.employeeid and n.registered = 1 and a.patient=pt.ssn);
 
--- select t.name as "Patient name",
---     from PATIENT t 
---     join APPOINTMENT a on a.patient=t.ssn
---     where COUNT(a.patient)>=2;
 
--- 3 rd 
 
 -- select t.name as "Patient Name",
 --     from PATIENT t
@@ -24,17 +20,14 @@ where a.prepnurse = n.employeeid and n.registered = 1 and a.patient=pt.ssn);
 --     where PROCEDUERE.cost>5000;
 
 
--- 3
+-- 3 query 
 select p.name, ph.name from UNDERGOES u 
 join PATIENT p on p.ssn=u.patient 
 join PHYSICIAN ph on p.pcp=ph.employeeid 
 join PROCEDURES mp on u.procedures=mp.code where mp.cost > 5000;
 
 
-select p.name from PHYSICIAN p
- where employeeid in (select  physician from UNDERGOES u 
- where not exists (select * from TRAINED_IN where treatment = procedures and physician=u.physician));
-
+-- 4 query 
  select p.name as physician,
   p.position as position, 
   pr.name_pro as proc, u.date, 
@@ -43,5 +36,13 @@ select p.name from PHYSICIAN p
   where u.patient = pt.ssn     and u.procedures = pr.code     and u.physician = p.employeeid  
   and pr.code = t.treatment     and p.employeeid = t.physician     and u.date > t.certificationexp;
 
--- 2.
+
+ -- 5 th query 
+ 
+select p.name from PHYSICIAN p
+ where employeeid in (select  physician from UNDERGOES u 
+ where not exists (select * from TRAINED_IN where treatment = procedures and physician=u.physician));
+
+
+
  
